2aaa8342205d956b5ad63d434f5d658c287015cb
Alex Collin <alex@collin.su>
2020-04-29 19:17:32 +0300

    Fix api url for docker

--- www/config/params.php
+++ www/config/params.php
@@ -4,5 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'apiUrl' => 'http://http-api:8001/v1',
+    'apiUrl' => 'http://127.0.0.1:8001/v1',
 ];
